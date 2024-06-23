04f7b01344000f85286fa9def2409ff21525ddab
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-23 19:22:37 +0100

    Change @hidden_word to array so it can be compared to @guess_word; initialize @guess_word as an array of underscores equal in length to@hidden_word

--- lib/main/game.rb
+++ lib/main/game.rb
@@ -3,12 +3,13 @@ require_relative "game/show_board"
 class Game
   def initialize
     dictionary = File.readlines("google-10000-english-no-swears.txt")
-    @hidden_word = dictionary[rand(1..10000)]
-    @guess_word
+    @hidden_word = dictionary[rand(1..10000)].chomp.chars
+    @guess_word = hidden_word.map {|i| i = "_"}
     @guessed_letters = []
     @wrong_guesses = 0
     @hangman = [" ", " ", " ", " ", " ", " "]
-    puts hidden_word
+    p hidden_word
+    p guess_word
     show_board
   end
   attr_accessor :hidden_word, :guess_word, :guessed_letters, :wrong_guesses, :hangman
