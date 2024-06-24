218ce1f881947717c459f65b0af1d9d0fbb9b1c6
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-24 08:15:30 +0100

    Add win checking and end-game messages to PlayGame

--- lib/main/game/play_game.rb
+++ lib/main/game/play_game.rb
@@ -1,7 +1,15 @@
 module PlayGame
   def play_game
-    while self.wrong_guesses < 7 do
+    while self.wrong_guesses < 6 do
       self.user_guess
+      if guess_word == hidden_word
+        break
+      end
+    end
+    if guess_word == hidden_word
+      puts "\nCongratulations! You win!"
+    else
+      puts "Oops! Game over! You lose!"
     end
   end
 end
